1cb4353fa9103fdb377b7a2c4808056f09a74712
Tom Fischer <tom.fischer@utn.de>
2026-07-07 09:35:45 +0200

    Skeleton

--- src/emergent_canonical_frame/engine/trainer.py
+++ src/emergent_canonical_frame/engine/trainer.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from hydra.core.hydra_config import HydraConfig
 from hydra.utils import instantiate, to_absolute_path
 
+from emergent_canonical_frame.data.loader import get_dataloader
 from emergent_canonical_frame.utils import seed_everything
 from emergent_canonical_frame.utils.ddp_utils import (
     init_ddp,
@@ -17,19 +18,16 @@ def train(cfg):
     distributed = is_dist_avail_and_initialized()
     world_size = get_world_size()
     rank = get_rank()
-
     seed_everything(cfg.seed, rank)
     run_dir = Path(HydraConfig.get().runtime.output_dir)
 
     dataset = instantiate(cfg.dataset, split="train")
-
+    dataloader = get_dataloader(dataset, cfg.batch_size, train=True, 
+        world_size=world_size, distributed=distributed, rank=rank)
 
     # Step 2: Initialize the model
-    # 
-    # Step 3: Initialize the optimizer
-    # 
-    # Step 4: Initialize the dataloaders
-    # 
-    # Step 5: Train the model
-
+    model = instantiate(cfg.model)
+    
+    optim = instantiate(cfg.optim, params=model.parameters())
+    
     pass
